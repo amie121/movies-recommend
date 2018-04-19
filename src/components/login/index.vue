c2d95f095743f6cab137fc0bac9594205141cbb6
amie121 <928611566@qq.com>
2018-04-19 22:12:50 +0800

    init

---
+++ src/components/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-wrapper">
+    <div class="login">
+      <img src="../../assets/images/login.jpeg" alt="login_page" class="login-img" width="400">
+      <div class="login-form">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss">
+.login-wrapper {
+  width: 100%;
+  height: 100%;
+  background-color: #eee;
+}
+.login {
+  display: inline-block;
+  margin-top: 60px;
+}
+</style>
